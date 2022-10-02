383f489beb4a8691e199252a485bd2e6849ec5d6
fatihgenc <gnc.fath@gmail.com>
2022-10-02 23:55:52 +0300

    add: new constants for url

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,6 @@
+// URL Related
 export const URL_REGEX =
   /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)?/gi;
+export const INVALID_URL_TEXT = 'You"ve entered an invalid url.';
+export const URL_INPUT_PLACEHOLDER =
+  '(bershka/amazon/hepsiburada/trendyol).com/your-dream-product';
