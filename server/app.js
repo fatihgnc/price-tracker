317abba0f63ac763431776ad3e064d427948462e
fatihgenc <gnc.fath@gmail.com>
2022-10-07 01:23:22 +0300

    change: endpoint url and method for getting price

--- server/app.js
+++ server/app.js
@@ -9,10 +9,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
-app.post('/', async (req, res) => {
+app.get('/api/', async (req, res) => {
+  const { url } = req.query;
+  if (!url) {
+    return res
+      .status(400)
+      .json({ error: true, message: 'An url must be provided.' });
+  }
   try {
-    console.log('here');
-    const price = await scrapePrice(req.body.url);
+    const price = await scrapePrice(url);
     return res.json({ price });
   } catch (error) {
     res.json({ error: error.message });
