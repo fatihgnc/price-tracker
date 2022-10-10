b485f132e35910adbfa5b5a65f4394d9d64a9fce
fatihgenc <gnc.fath@gmail.com>
2022-10-10 23:10:16 +0300

    add: new constants

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -12,4 +12,11 @@ module.exports = {
   ZARA: 'zara',
   USER_AGENT:
     'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Mobile Safari/537.36',
+
+  ROOT_PATH: '/',
+  REGISTER_PATH: '/register',
+  LOGIN_PATH: '/login',
+
+  DB_URI: 'mongodb://localhost:27017/price-tracker',
+  PORT: process.env.PORT || 3001,
 };
